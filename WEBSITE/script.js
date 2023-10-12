292dc71c685c910c2d616746d7a2a7f405a734dd
GUNAL SARAVANAN <130141725+GunalSaravanan@users.noreply.github.com>
2023-10-12 21:11:27 +0530

    Add files via upload

---
+++ WEBSITE/script.js
@@ -0,0 +1,29 @@
+$(function () {
+  var n = "#nav";
+  var no = ".nav-items";
+  $(n).click(function () {
+    if ($(no).hasClass("nav-open")) {
+      $(no).animate({ height: 0 }, 300);
+      setTimeout(function () {
+        $(no).removeAttr('style').removeClass("nav-open");
+      }, 320);
+    } else {
+      var h = $(no).css("height", "auto").height();
+      $(no).height(0).animate({ height: h }, 300);
+      setTimeout(function () {
+        $(no).removeAttr('style').addClass("nav-open");
+      }, 320);
+    }
+  });
+});
+$(document).ready(function () {
+  var navbar = $(".navbar");
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) { // Adjust the scroll threshold as needed
+      navbar.addClass("fixed");
+    } else {
+      navbar.removeClass("fixed");
+    }
+  });
+});
